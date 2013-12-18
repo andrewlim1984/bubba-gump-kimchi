afa4007a169239ee7f99ea31116e1a409b6b944e
andrewlim1984@gmail.com <andrewlim1984@gmail.com>
2013-12-17 18:00:13 -0800

    i have a working version of my kimchi app

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-var express  = require('express');
-var app      = express(); 					
-var mongoose = require('mongoose'); 					
-var port  	 = process.env.PORT || 5000; 				
-var database = require('./config/database');
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  Firebase = require('firebase'),
+  path = require('path');
+ var app = module.exports = express();
 
-mongoose.connect(database.url, function(err) {
-  if (err) return err;
-console.log('successfully connected to mongoDB!');
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+
+app.get('/api/posts', api.posts);
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+app.get('*', routes.index);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-app.configure(function() {
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.logger('dev')); 						
-	app.use(express.bodyParser()); 							
-	app.use(express.methodOverride()); 						
-});
-
-require('./app/routes')(app);
-
-app.listen(port);
-console.log("App listening on port " + port);
